b5bc60648f788e571f38c4478a293a438f69247c
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-17 22:36:16 +0100

    implement the addToDelivery  method in CandyDAO

--- src/main/java/com/example/demo/DAO/CandyDAOImpl.java
+++ src/main/java/com/example/demo/DAO/CandyDAOImpl.java
@@ -4,6 +4,7 @@ import com.example.demo.entity.CandyData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -22,6 +23,10 @@ public class CandyDAOImpl implements CandyDAO{
 
     private static final String SELECT_ALL_CANDY = "SELECT * FROM candy";
 
+    private static final String INSERT_DELIVERY =
+            "INSERT INTO candy_delivery (candy_id, delivery_id) " +
+                    "VALUES (:" + CANDY_ID + ", :" + DELIVERY_ID + ")";
+
     private static final RowMapper<CandyData> candyDataRowMapper =
             new BeanPropertyRowMapper<>(CandyData.class);
 
@@ -32,6 +37,9 @@ public class CandyDAOImpl implements CandyDAO{
 
     @Override
     public void addToDelivery(Long candyId, Long deliveryid) {
+
+        jdbcTemplate.update(INSERT_DELIVERY, new MapSqlParameterSource()
+                .addValue(CANDY_ID,candyId).addValue(DELIVERY_ID,deliveryid));
 
     }
 

13c2d9a26dd758a3ae4c8b3b9fccb39950006cfc
Ahmed Khalil Khalef <46876471+a2khalil@users.noreply.github.com>
2021-03-17 22:34:09 +0100

    implement the list() method in CandyDAO

--- src/main/java/com/example/demo/DAO/CandyDAOImpl.java
+++ src/main/java/com/example/demo/DAO/CandyDAOImpl.java
@@ -1,4 +1,42 @@
 package com.example.demo.DAO;
 
-public class CandyDAOImpl {
+import com.example.demo.entity.CandyData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class CandyDAOImpl implements CandyDAO{
+
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static final String CANDY_ID = "candyId";
+    private static final String DELIVERY_ID = "deliveryId";
+
+    private static final String SELECT_ALL_CANDY = "SELECT * FROM candy";
+
+    private static final RowMapper<CandyData> candyDataRowMapper =
+            new BeanPropertyRowMapper<>(CandyData.class);
+
+    @Override
+    public List<CandyData> list() {
+        return jdbcTemplate.query(SELECT_ALL_CANDY, candyDataRowMapper);
+    }
+
+    @Override
+    public void addToDelivery(Long candyId, Long deliveryid) {
+
+    }
+
+    @Override
+    public List<CandyData> findByDelivery(Long deliveryId) {
+        return null;
+    }
 }
